f2ac38956f1ccc91fb757c4a7e0455a899bd472c
hou <houshunwei@houdeMacBook-Pro.local>
2015-03-31 10:53:48 +0800

    Initial Commit

---
+++ zDateMatter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zDateMatter
+//
+//  Created by hou on 15/3/31.
+//  Copyright (c) 2015年 hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
